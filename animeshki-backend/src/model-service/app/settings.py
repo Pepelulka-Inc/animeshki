ae15b764ecda5ee49a317ab96e3c491888e81201
lockr <doly2004e@yandex.ru>
2024-12-27 03:08:42 +0300

    Change port to env

--- animeshki-backend/src/model-service/app/settings.py
+++ animeshki-backend/src/model-service/app/settings.py
@@ -3,14 +3,14 @@ import logging
 from typing import Dict, List
 
 logging.basicConfig(level=logging.INFO)
-logger: logging.Logger = logging.getLogger("minio-service.settings")
+logger: logging.Logger = logging.getLogger("model-service.settings")
 
 USE_MINIO: bool = True if os.getenv("USE_MINIO", False) == "True" else False
 DEBUG: bool = True if os.getenv("DEBUG", False) == "True" else False
 # Настройки сервиса
-PORT: int = 8000
+PORT: int = os.getenv("PORT", 8000)
 
-# Настройки MINIO
+# Настройки MODEL
 MODEL_SETTINGS: Dict[str, str] = {
     "EPOCHS": os.getenv("MODEL_TRAIN_EPOCHS", 100),
     "NO_COMPONENTS": os.getenv("MODEL_TRAIN_NO_COMPONENTS", 10),

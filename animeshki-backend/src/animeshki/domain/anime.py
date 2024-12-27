37f0c566f24cf88735e4c16d61a656d2594b79fe
DDemagoGG <lyadiez@mail.ru>
2024-12-27 12:58:05 +0300

    Animeshki V.1.0

--- animeshki-backend/src/animeshki/domain/anime.py
+++ animeshki-backend/src/animeshki/domain/anime.py
@@ -5,12 +5,12 @@ class AnimeModel(BaseModel):
     anime_id: str
     title: str
     description: str
-    picture_minio_path: str
+    picture_minio_path: str | None
     mal_id: int | None
 
 
 class AnimeCreateModel(BaseModel):
     title: str
     description: str
-    picture_minio_path: str
+    picture_minio_path: str | None
     mal_id: int | None
